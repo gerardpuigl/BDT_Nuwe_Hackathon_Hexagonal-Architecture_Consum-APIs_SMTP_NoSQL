3b3075f357d1741d102a0af78f97be8e9c4412bc
Gerard <gerardpuig31@hotmail.com>
2021-06-12 11:00:20 +0200

    Project configuration & TASK1

---
+++ src/main/java/io/nuwe/hackatonMWC/HackatonMwcApplication.java
@@ -0,0 +1,13 @@
+package io.nuwe.hackatonMWC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonMwcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonMwcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/nuwe/hackatonMWC/HackatonMwcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nuwe.hackatonMWC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonMwcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
